d5d580702c6a1ed32ecebd2dbc043c3797ea73af
Victor-R <victor_ribeiro.pira@hotmail.com>
2020-05-19 19:55:51 -0300

    Implemented list and create flows for transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +19,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(
+      (result: Balance, currentValue) => {
+        const newIncome =
+          currentValue.type === 'income'
+            ? result.income + currentValue.value
+            : result.income;
+        const newOutcome =
+          currentValue.type === 'outcome'
+            ? result.outcome + currentValue.value
+            : result.outcome;
+
+        return {
+          income: newIncome,
+          outcome: newOutcome,
+          total: newIncome - newOutcome,
+        } as Balance;
+      },
+      { income: 0, outcome: 0, total: 0 } as Balance,
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
